63a597267360127600168264f5f363721db2f8fb
Rebekah Patrice <rebekahp@ox-l3.msk.21-school.ru>
2026-07-22 18:12:47 +0300

    D02: Quest 4

---
+++ D02T02_ID_1577482-1/src/important_function.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void){
+    double x;
+    double y;
+    int input;
+    input = scanf("%lf", &x);
+    if (input!=1){
+        printf("n/a");
+        return 0;
+
+    }
+    y = 7e-3 * pow(x,4) + 
+        ((22.8 * pow(x,(1/3)) - 1e3) * x + 3) / (pow(x,2)/ 2) - x * pow((10 + x),(2/x)) - 1.01;
+     printf("%.1f", y);
+}
