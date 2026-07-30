6cac94fe7e5cd7ecadbdeff42c5496afa28920de
Rebekah Patrice <rebekahp@ox-r6.msk.21-school.ru>
2026-07-30 19:22:51 +0300

    Sync D10T07_ID_1577488-1

--- D10T07_ID_1577488-1/src/dmanager_module.c
+++ D10T07_ID_1577488-1/src/dmanager_module.c
@@ -8,12 +8,20 @@
 #define MAX_ID_SEED 10000
 
 void initialize_doors(struct door* doors);
+void close_all_doors(struct door* doors);
+void sort_doors(struct door* doors);
 
 int main()
 {
     struct door doors[DOORS_COUNT];
     
     initialize_doors(doors);
+    sort_doors(doors);
+    close_all_doors(doors);
+
+    for (int i = 0; i < DOORS_COUNT; i++) {
+        printf("%d, %d ", doors[i].id, doors[i].status);
+    }
     
 
 
@@ -32,5 +40,23 @@ void initialize_doors(struct door* doors)
     {
         doors[i].id = (i + seed) % DOORS_COUNT;
         doors[i].status = rand() % 2;
+    }
+}
+
+void close_all_doors(struct door* doors) {
+    for (int i = 0; i < DOORS_COUNT; i++) {
+        doors[i].status = 0;
+    }
+}
+
+void sort_doors(struct door* doors) {
+    for (int i = 0; i < DOORS_COUNT - 1; i++) {
+        for (int j = 0; j < DOORS_COUNT - 1 - i; j++) {
+            if (doors[j].id > doors[j + 1].id) {
+                struct door tmp = doors[j];
+                doors[j] = doors[j + 1];
+                doors[j + 1] = tmp;
+            }
+        }
     }
 }
---
+++ D10T07_ID_1577488-1/src/door_struct.h
@@ -0,0 +1,10 @@
+#ifndef DOOR_STRUCT_H
+#define DOOR_STRUCT_H
+
+typedef struct door {
+    int id;
+    int status;
+    
+} door;
+
+#endif
